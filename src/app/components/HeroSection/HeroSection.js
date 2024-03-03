ef9226c5cbd2162393ae193f2a3a3ccb754399dd
volokhinn <csgomx@mail.ru>
2024-03-03 23:15:15 +0700

    initial

---
+++ src/app/components/HeroSection/HeroSection.js
@@ -0,0 +1,27 @@
+import styles from './HeroSection.module.scss';
+import Link from 'next/link';
+import background from '../../../../public/home/herobg.png';
+
+export const HeroSection = () => {
+  return (
+    <div className={styles.main}>
+      <h1>
+        Powerful for developers.<br></br>Fast for everyone.
+      </h1>
+
+      <span className={styles.subtitle}>
+        Bring blockchain to the people. Solana supports experiences <br></br> for power users, new
+        consumers, and everyone in between.
+      </span>
+
+      <div className={styles.btngroup}>
+        <Link href="/3">
+          <button className={styles.btngradient}>Start building</button>
+        </Link>
+        <Link href="/3">
+          <button className={styles.btnvoid}>Read Docs</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
